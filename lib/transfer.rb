eef634f4efbb2c13e3cb26d2e04760da0cb964a0
Kevin Boston <kbos22@gmail.com>
2020-06-01 13:34:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,12 @@ class Transfer
      sender.balance -= amount
      receiver.balance += amount
      self.status = "complete"
+   else 
+     reject_transfer
    end
    
+ end
+ def reject_transfer
+   "Transaction rejected. Please check your account balance."
  end
 end
