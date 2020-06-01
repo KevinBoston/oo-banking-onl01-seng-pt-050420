dfa5d04fa5625f9522100b28868ee885cf3d5db0
Kevin Boston <kbos22@gmail.com>
2020-06-01 13:29:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
    if valid? && sender.balance >= amount && self.status = "pending"
      @sender.balance -= @amount
      @receiver.balance += @amount
-     @status = "complete"
+     self.status = "complete"
    end
    
  end
